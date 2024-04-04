a35a1e8ba1d0082d1d68611dd3e80eab56229c13
Woffes <squidg4ce@gmail.com>
2024-04-04 14:32:20 +0200

    import the pic of le cat, so its drawn in le house
    you can se the picture of le cat, just like le dog, in le house

--- Cat.js
+++ Cat.js
@@ -1,10 +1,10 @@
 class Cat extends Pet {
-    constructor(name){
-        super(name, "cat")
+    constructor(name, catImage){
+        super(name, "cat", catImage)
     }
 
-    drawAnimal(posX, posY){
-        
+    drawAnimal(posX, posY, size){
+        image(this.petImage, posX, posY, size, size)
     }
 
     pet() {
--- sketch.js
+++ sketch.js
@@ -1,19 +1,24 @@
 let backgroundImg;
 let dog;
-let dogImg
+let dogImg;
+let cat;
+let catImg;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   dog = new Dog("Emil", dogImg)
+  cat = new Cat("casper", catImg)
 }
 
 function draw() {
   image(backgroundImg, 0, 0, width, height);
   dog.drawAnimal(width / 2, height * 2/3, 200)
+  cat.drawAnimal(width / 3, height * 2/3, 150)
 }
 
 function preload() {
   backgroundImg = loadImage('images/le house.jpeg');
   dogImg = loadImage('images/le dog.png')
+  catImg = loadImage('images/le cat.png')
 }
