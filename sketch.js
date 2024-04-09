f1c050e2fab7938e93e4dd9b4a2e380438f0cccb
AugmentedDuck <anders.m.matzen@gmail.com>
2024-04-09 13:07:53 +0200

    Made pets walk around

--- sketch.js
+++ sketch.js
@@ -3,19 +3,21 @@ let dog;
 let dogImg;
 let cat;
 let catImg;
-
+let dogX;
+let catX;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   dog = new Dog("Emil", dogImg)
   cat = new Cat("casper", catImg)
+  dogX = width / 2
+  catX = width / 3
 }
 
 function draw() {
   image(backgroundImg, 0, 0, width, height);
-  dog.drawAnimal(width / 2, height * 2/3, min(width,height)/3)
-  cat.drawAnimal(width / 3, height * 2/3, min(width,height)/4)
-  text(`DOG AFFINITY: ${dog.affinity}\nCAT AFFINITY: ${cat.affinity}`,width/20, height/20)
+  walkAround()
+  text(`DOG AFFINITY: ${dog.affinity}\nCAT AFFINITY: ${cat.affinity}`,width/20, height/20);
 }
 
 function preload() {
@@ -32,4 +34,24 @@ function mousePressed() {
   if(cat.wasPetClicked()) {
     cat.pet()
   }
+}
+
+function walkAround() {
+  //DOG
+  let deltaX = random(-10,10)
+  dogX += deltaX;
+  //Check if sides are hit
+  if (dogX < 0) dogX = 0;
+  if (dogX > width - dog.size) dogX = width - dog.size
+  
+  dog.drawAnimal(dogX, height * 2/3, min(width,height)/3);
+
+  //CAT
+  deltaX = random(-5,5)
+  catX += deltaX
+  //Check if sides are hit
+  if (catX < 0) catX = 0;
+  if (catX > width - cat.size) catX = width - cat.size
+
+  cat.drawAnimal(catX, height * 2/3, min(width,height)/4);
 }
