f7651c027e144d68f27adf7dd9a521be477d1697
AugmentedDuck <anders.m.matzen@gmail.com>
2024-04-23 13:35:14 +0200

    Added a second timer

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ let dogX;
 let catX;
 let time = 0;
 let foodImg;
+let firstSave = (window.localStorage.getItem(`timeSaved`) === null) ? Date.now() : window.localStorage.getItem(`timeSaved`);
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,13 +15,14 @@ function setup() {
   cat = new Cat("Casper", catImg, "c46360f6-0c0b-4de3-870b-ea98a720a676")
   dogX = width / 2
   catX = width / 3
+  window.localStorage.setItem(`timeSaved`, firstSave)
 }
 
 function draw() {
   image(backgroundImg, 0, 0, width, height);
   image(foodImg, width * 8.75 / 10, height / 10, min(width,height)/5, min(width,height)/5)
 
-  text(`DOG AFFINITY: ${dog.affinity}\nCAT AFFINITY: ${cat.affinity}`,width/20, height/20);
+  text(`DOG AFFINITY: ${dog.affinity}\nCAT AFFINITY: ${cat.affinity}\nSeconds played: ${(Date.now() - firstSave)/1000}`,width/20, height/20);
 
   dog.walkAround()
   cat.walkAround()
