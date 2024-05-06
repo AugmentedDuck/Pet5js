15bfa5213044cc9b0a5cf820e871e28db381b447
AugmentedDuck <anders.m.matzen@gmail.com>
2024-05-06 16:44:18 +0200

    Fixed new pet function

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ let catX;
 let time = 0;
 let foodImg;
 let firstSave = (window.localStorage.getItem(`timeSaved`) === null) ? Date.now() : window.localStorage.getItem(`timeSaved`);
+let bestTime = (window.localStorage.getItem(`bestSaved`) === null) ? 0 : window.localStorage.getItem(`bestSaved`);
 let liveAnimals = [];
 
 function setup() {
@@ -29,7 +30,7 @@ function draw() {
   image(backgroundImg, 0, 0, width, height);
   image(foodImg, width * 8.75 / 10, height / 10, min(width,height)/5, min(width,height)/5)
 
-  text(`Seconds played: ${(Date.now() - firstSave)/1000}`,width/20, height/20);
+  text(`Seconds played: ${(Date.now() - firstSave)/1000}\nBest: ${bestTime}`,width/20, height/20);
 
   for (const dog of dogs) {
     dog.walkAround()
@@ -98,40 +99,52 @@ function saveAnimals() {
   for (const animal of liveAnimals) {
     savefile += animal + " "
   }
-  savefile.trim()
+  savefile = savefile.trim()
 
-  console.log(savefile)
   window.localStorage.setItem("allIDs", savefile)
 }
 
 function removeAnimal(id) {
-  liveAnimals.splice(liveAnimals.indexOf(id))
+  console.log(liveAnimals)
+
+  liveAnimals.splice(liveAnimals.indexOf(id), 1)
   saveAnimals();
+  
+  console.log(liveAnimals)
 }
 
 function addAnimal() {
-  let newID = crypto.randomUUID()
+  let newID = crypto.randomUUID().toString()
   if (Math.round(Math.random()) == 1) {
     liveAnimals.push("D" + newID)
     dogs.push(new Dog('Name', dogImg, newID))
+    dogs[dogs.length - 1].eat()
   } else {
     liveAnimals.push("C" + newID)
     cats.push(new Cat('Name2',catImg,newID))
+    cats[cats.length - 1].eat()
   }
+
+  window.localStorage.setItem(`timeSaved`, Date.now())
+  bestTime = (Date.now() - firstSave > bestTime) ? (Date.now() - firstSave) / 1000 : bestTime;
+  window.localStorage.setItem(`bestSaved`, bestTime)
+  firstSave = Date.now();
 
   saveAnimals();
 }
 
 function loadAnimals() {
-  let animalIDAll = window.localStorage.getItem('allIDs')
+  let animalIDAll = (window.localStorage.getItem(`allIDs`) === null) ? "D" + crypto.randomUUID().toString() : window.localStorage.getItem(`allIDs`);
 
-  liveAnimals = animalIDAll.split(' ');
+  liveAnimals = animalIDAll.split(' ').filter(id => id !== '');
   for (const animal of liveAnimals) {
     let animalID = animal.slice(1, animal.length - 1)
     if (animal.charAt(0) == "D") {
-      dogs.push(new Dog('Emil', dogImg, animalID))
-    } else {
-      cats.push(new Cat('Casper', catImg, animalID))
+      dogs.push(new Dog('Name', dogImg, animalID))
+    } else if (animal.charAt(0) == "C"){
+      cats.push(new Cat('Name2', catImg, animalID))
     }
   }
+
+  console.log(liveAnimals)
 }
