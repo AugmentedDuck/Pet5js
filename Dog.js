677b4f8fdb5eee49d7313e1602533335b9dc313b
Woffes <squidg4ce@gmail.com>
2024-05-07 13:12:23 +0200

    le ball fetch le works...      le

--- Dog.js
+++ Dog.js
@@ -10,17 +10,19 @@ class Dog extends Pet {
     walkTo(newX, ballStandingStill) {
         if (ballStandingStill) {
             this.canWalkAround = true
-        }
-        this.canWalkAround = false
-        this.targetX = newX
-        if (!this.isDead) {
-            if(round(this.targetX) == round(this.posX) || !this.targetX) {
-                this.canWalkAround = true
+        } else {
+            this.canWalkAround = false
+            this.targetX = newX
+            if (!this.isDead) {
+                if(round(this.targetX) == round(this.posX) || !this.targetX) {
+                    this.canWalkAround = true
+                }
+                this.posX = lerp(this.posX, this.targetX, 0.05)
+                  
+                this.drawAnimal(this.posX, height * 2/3, min(width,height)/3);
             }
-            this.posX = lerp(this.posX, this.targetX, 0.05)
-              
-            this.drawAnimal(this.posX, height * 2/3, min(width,height)/3);
         }
+        
     }
 
     playFetch() {
